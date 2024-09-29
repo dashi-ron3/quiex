572e0c4d0ee6281d6cb928bc1dbab3b45e4c88cb
rnn-gnzls <116857824+rnn-gnzls@users.noreply.github.com>
2024-09-29 07:01:29 +0000

    QuiEx

---
+++ database/quiex-users.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE quiex;
+
+USE quiex;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+SELECT * from users;
