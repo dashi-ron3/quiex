d7ab2f1f8eac896f59d8ae53e16e60cf2e3dff04
Hannah Belen <87771420+hvpbln@users.noreply.github.com>
2024-10-04 21:42:58 +0800

    minor changes

--- teacher-assessments.php
+++ teacher-assessments.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Subject Assessments</title>
-    <link rel="stylesheet" href="assessment-style.css">
+    <link rel="stylesheet" href="css/assessment-style.css">
 </head>
 
 <body>
