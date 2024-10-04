e41ada147e5c99946b36bd5b90ebe89d2966ab6a
Hannah Belen <87771420+hvpbln@users.noreply.github.com>
2024-10-04 22:19:11 +0800

    randomizer function sample

---
+++ randomizer.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quiz Randomizer</title>
+    <link rel="stylesheet" href="css/randomizer.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet"/>
+</head>
+<body>
+
+    <button id="randomizeButton" class="randomizer">
+        <img src="assets/randomizer.png" alt="Randomize Items">
+    </button>
+    
+    <div id="quizContainer"></div>
+
+    <script src="javascript/randomizer.js"></script>
+</body>
+</html>
