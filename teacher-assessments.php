f233cc6ba3e1586a91a96140d7f7b08642261dd2
dashi-ron3 <ronwela12@gmail.com>
2024-10-19 09:58:00 +0800

    Update teacher-assessments.php

--- teacher-assessments.php
+++ teacher-assessments.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Subject Assessments</title>
+    <title>Teacher Assessments</title>
     <link rel="stylesheet" href="css/assessment-style.css">
 </head>
 
@@ -22,6 +22,7 @@
     </div>
 
     <div class="container">
+
         <div class="sidebar">
             <div class="subject">
                 <img src="assets/sub-folder.png" alt="biology folder">
@@ -40,56 +41,41 @@
         <div class="content">
             <h1>BIOLOGY</h1>
 
-            <div class="assessment">
-                <div class="last-used">LAST USED ON: 2024/10/08</div>
-                <div class="header">
-                    <div class="title-status">
-                        <div class="title"><strong>Assessment Title:</strong> Lorem Ipsum</div>
-                        <div class="status"><strong>Status:</strong> Done</div>
+            <?php
+            $conn = mysqli_connect("localhost", "root", "15a5m249ph", "testing");
+            if (mysqli_connect_errno()) {
+                die("Connection failed: " . mysqli_connect_error());
+            }
+
+            $sql = "SELECT title, status, lastUsed, descrip FROM assessments";
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+            ?>
+                    <div class="assessment">
+                        <div class="last-used">LAST USED ON: <?php echo htmlspecialchars($row['lastUsed']); ?></div>
+                        <div class="header">
+                            <div class="title-status">
+                                <div class="title"><strong>Assessment Title:</strong> <?php echo htmlspecialchars($row['title']); ?></div>
+                                <div class="status"><strong>Status:</strong> <?php echo htmlspecialchars($row['status']); ?></div>
+                            </div>
+                            <a href="#" class="edit"><strong>Edit</strong></a>
+                        </div>
+                        <p class="details"><?php echo htmlspecialchars($row['descrip']); ?></p>
+                        <div class="share">
+                            <label for="share-<?php echo $row['title']; ?>">SHARE TO PUBLIC: </label>
+                            <input type="checkbox" id="share-<?php echo $row['title']; ?>">
+                        </div>
                     </div>
+            <?php
+                }
+            } else {
+                echo "<p>No assessments found.</p>";
+            }
 
-                    <a href="#" class="edit"><strong>Edit</strong></a>
-                </div>
-                <p class="details">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id et porta fringilla neque platea leo...</p>
-                <div class="share">
-                    <label for="share1">SHARE TO PUBLIC: </label>
-                    <input type="checkbox" id="share1" checked>
-                </div>
-            </div>
-
-            <div class="assessment">
-                <div class="last-used">LAST USED ON: 2024/10/08</div>
-                <div class="header">
-                    <div class="title-status">
-                        <div class="title"><strong>Assessment Title:</strong> Lorem Ipsum</div>
-                        <div class="status"><strong>Status:</strong> Done</div>
-                    </div>
-
-                    <a href="#" class="edit"><strong>Edit</strong></a>
-                </div>
-                <p class="details">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id et porta fringilla neque platea leo...</p>
-                <div class="share">
-                    <label for="share2">SHARE TO PUBLIC: </label>
-                    <input type="checkbox" id="share2">
-                </div>
-            </div>
-
-            <div class="assessment">
-                <div class="last-used">LAST USED ON: 2024/10/08</div>
-                <div class="header">
-                    <div class="title-status">
-                        <div class="title"><strong>Assessment Title:</strong> Lorem Ipsum</div>
-                        <div class="status"><strong>Status:</strong> Done</div>
-                    </div>
-
-                    <a href="#" class="edit"><strong>Edit</strong></a>
-                </div>
-                <p class="details">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id et porta fringilla neque platea leo...</p>
-                <div class="share">
-                    <label for="share3">SHARE TO PUBLIC: </label>
-                    <input type="checkbox" id="share3">
-                </div>
-            </div>
+            $conn->close();
+            ?>
         </div>
     </div>
 </body>
