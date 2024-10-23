14977f770dcb504bc59918eb18562a0cbb68c5f9
riyan <116857824+rnn-gnzls@users.noreply.github.com>
2024-10-23 15:45:59 +0800

    Update connection.php

--- config/connection.php
+++ config/connection.php
@@ -4,12 +4,18 @@ if (session_status() == PHP_SESSION_NONE) {
 }
 
 // replace what needs to be replaced
+$dsn = 'mysql:host=localhost;dbname=quiex';
 $servername = "localhost";
 $db_username = "root";
 $db_password = "yannigonzales";
 $dbname = "quiex";
 
 $conn = new mysqli($servername, $db_username, $db_password, $dbname);
+
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
