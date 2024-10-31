a802f4879f48c264d698c57135739cc9886527dd
riyan <116857824+rnn-gnzls@users.noreply.github.com>
2024-10-31 23:24:37 +0800

    proper title structure

--- rapid-quiz.php
+++ rapid-quiz.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="assets/logo-quiex.ico"/>
-    <title>QuiEx</title>
+    <title>Rapid Quiz | QuiEx</title>
     <link rel="stylesheet" href="css/rapid-quiz.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet"/>
     <style>
