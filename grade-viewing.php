c324f6b6b59fdf8ea4b145c6402f975629dbcea7
kdalosa <karsheenadalosa@gmail.com>
2024-10-31 14:22:18 +0800

    added logo icon

--- grade-viewing.php
+++ grade-viewing.php
@@ -56,6 +56,7 @@ $result = $conn->query($sql);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="assets/logo-quiex.ico"/>
     <title>QuiEx</title>
     <link rel="stylesheet" href="css/grades.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
