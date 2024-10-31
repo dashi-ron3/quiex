c0d7770667400b95f083838900638a0d52a3ed75
kdalosa <karsheenadalosa@gmail.com>
2024-10-31 14:21:55 +0800

    added logo icon

--- rapid-quiz.php
+++ rapid-quiz.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="assets/logo-quiex.ico"/>
     <title>QuiEx</title>
     <link rel="stylesheet" href="css/rapid-quiz.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet"/>
