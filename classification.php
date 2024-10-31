8b43db269f7d188aded32a978b7849d509d2dda0
riyan <116857824+rnn-gnzls@users.noreply.github.com>
2024-10-31 23:22:29 +0800

    proper title structure

--- classification.php
+++ classification.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="QuiEx Classification Page">
-    <title>QuiEx | Classification</title>
+    <title>Classification | QuiEx</title>
     <link rel="stylesheet" href="css/classification.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet"/>
     <script src="javascript/classification.js" defer></script>
